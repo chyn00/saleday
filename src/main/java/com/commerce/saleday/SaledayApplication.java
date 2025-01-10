df00f152ce26a5729857f50345bb89291d42023b
신충현 <ybj0749@gmail.com>
2025-01-10 16:05:12 +0900

    Initial Project

---
+++ src/main/java/com/commerce/saleday/SaledayApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.saleday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaledayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaledayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerce/saleday/SaledayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.saleday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaledayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
