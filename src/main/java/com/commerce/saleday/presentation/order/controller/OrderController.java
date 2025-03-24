1571b8ac7578d8e21e26e275d655d8650945e747
chyn00 <chris@sinchunghyeon-ui-MacBookPro.local>
2025-03-24 23:11:01 +0900

    add. 1. 아이템 객체 생성 2. 구조 정리

--- src/main/java/com/commerce/saleday/presentation/order/controller/OrderController.java
+++ src/main/java/com/commerce/saleday/presentation/order/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.commerce.saleday.presentation.order.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
