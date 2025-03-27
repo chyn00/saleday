fdac6abccca8086f4e98b72e31176781b2574122
chyn00 <chris@sinchunghyeon-ui-MacBookPro.local>
2025-03-27 16:51:54 +0900

    add. 테스트 코드 추가 1. item 테스트 코드 추가 2. review 테스트 코드 추가

--- src/main/java/com/commerce/saleday/domain/order/Entity/Orders.java
+++ src/main/java/com/commerce/saleday/domain/order/Entity/Orders.java
@@ -7,10 +7,10 @@ import lombok.*;
 @Entity
 @Getter
 @Builder
-@Table(name = "order")
+@Table(name = "orders")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)// 하이버네이트 Proxy에서 사용하도록 단계 조정
 @AllArgsConstructor(access = AccessLevel.PRIVATE)// 외부에서 사용안할거지만, Lombok기본 Builder 생성에 필요
-public class Order extends BaseEntity {
+public class Orders extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;// 고유번호가 아닌, +1로 생성되는 id
