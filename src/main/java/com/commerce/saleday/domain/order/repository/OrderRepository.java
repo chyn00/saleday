c9fe0a42e7ec96b9f1c78d05e0ed864e78ca102a
chyn00 <ybj0749@gmail.com>
2025-04-01 21:31:04 +0900

    add. 주문 인터페이스 개발 및 설계

--- src/main/java/com/commerce/saleday/domain/order/repository/OrderRepository.java
+++ src/main/java/com/commerce/saleday/domain/order/repository/OrderRepository.java
@@ -1,5 +1,9 @@
 package com.commerce.saleday.domain.order.repository;
 
+import com.commerce.saleday.domain.order.model.Orders;
+import java.util.Optional;
+
 public interface OrderRepository {
 
+  Optional<Orders> findOrderByCode(String orderCode);
 }
