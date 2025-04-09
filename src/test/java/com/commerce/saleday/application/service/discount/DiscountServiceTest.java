9a1ee7a7919c8058b3d67189793a4b56d267fa12
chyn00 <ybj0749@gmail.com>
2025-04-09 16:08:05 +0900

    add. DiscountService testCode 추가

--- src/test/java/com/commerce/saleday/application/service/discount/DiscountServiceTest.java
+++ src/test/java/com/commerce/saleday/application/service/discount/DiscountServiceTest.java
@@ -1,16 +1,30 @@
 package com.commerce.saleday.application.service.discount;
 
-import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.commerce.saleday.domain.discount.model.DiscountResult;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Transactional
 class DiscountServiceTest {
+
+  @Autowired
+  DiscountService discountService;
 
   @Test
   void getDiscountResult() {
     //given
+    double itemPrice = 23000;
 
     //when
+    DiscountResult discountResult = discountService.getDiscountResult(itemPrice);
 
     //then
+    assertThat(discountResult.getDiscountAmount()).isEqualTo(2000);
 
   }
 }
