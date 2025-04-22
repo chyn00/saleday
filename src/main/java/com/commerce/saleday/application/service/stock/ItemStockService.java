81d833355d16c487032a8ecf8d917ca6153cfb9c
chyn00 <ybj0749@gmail.com>
2025-04-23 00:07:30 +0900

    add. 테스트에 로그 추가

--- src/main/java/com/commerce/saleday/application/service/stock/ItemStockService.java
+++ src/main/java/com/commerce/saleday/application/service/stock/ItemStockService.java
@@ -3,6 +3,7 @@ package com.commerce.saleday.application.service.stock;
 import com.commerce.saleday.domain.stock.model.ItemStock;
 import com.commerce.saleday.domain.stock.port.ItemStockPort;
 import com.commerce.saleday.domain.stock.repository.ItemStockRepository;
+import jodd.util.StringUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,12 +20,13 @@ public class ItemStockService {
   @Transactional
   public Long setUpItemStock(String itemCode, ItemStock itemStock) {
 
-    Long remainingStock = itemStockPort.getItemStock(itemCode);
-
-    //남은 재고 수량이 있을 경우
-    if(remainingStock > 0){
+    String remaining = itemStockPort.getItemStock(itemCode);
+    if (StringUtil.isNotEmpty(remaining)) {
+      //남은 재고 수량이 있을 경우
+      long remainingStock = Long.parseLong(remaining);
       itemStockPort.setInitialItemStock(itemCode, itemStock.getQuantity() + remainingStock);
-    }else{//남은 재고 수량이 없을 경우
+    } else {
+      //남은 재고 수량이 없을 경우
       itemStockPort.setInitialItemStock(itemCode, itemStock.getQuantity());
     }
 
@@ -32,12 +34,10 @@ public class ItemStockService {
   }
 
   /**
-   * 재고 수량이 감소된다. 레디스를 사용해도 select을 먼저할 경우, 동시성 문제에서 자유롭지 못하다.
-   * 싱글쓰레드 atomic 기반의 decrement를 사용하여 원자성을 확보하고 정해진 수량 아래일 경우 로직을 진행하지 않도록 한다.
-   * 레디스 decr 및 진행 가능 여부 확인 함수
-   * 레디스에서 확인 가능 하면 order 처리하나, 고 트래픽의 경우 동시성 부하가 심할 수 있어서
-   * 카프카 벌크(배치) 처리로 나눠서 부하 적용 필요
-   * 특성상 decrement, count 가 같이 일어나야하는 거라서 하나의 method 로 통일
+   * 재고 수량이 감소된다. 레디스를 사용해도 select을 먼저할 경우, 동시성 문제에서 자유롭지 못하다. 싱글쓰레드 atomic 기반의 decrement를 사용하여 원자성을
+   * 확보하고 정해진 수량 아래일 경우 로직을 진행하지 않도록 한다. 레디스 decr 및 진행 가능 여부 확인 함수 레디스에서 확인 가능 하면 order 처리하나, 고 트래픽의
+   * 경우 동시성 부하가 심할 수 있어서 카프카 벌크(배치) 처리로 나눠서 부하 적용 필요 특성상 decrement, count 가 같이 일어나야하는 거라서 하나의 method
+   * 로 통일
    */
   public Long decrementAndCountItemStock(String itemCode) {
 
