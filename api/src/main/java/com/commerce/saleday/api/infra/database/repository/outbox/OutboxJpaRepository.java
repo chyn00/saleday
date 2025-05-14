fca683a3254ef422a0a432f32116403d2d70e193
chyn00 <ybj0749@gmail.com>
2025-05-14 22:29:38 +0900

    add. outbox init, success 데이터 주기적 삭제 로직 추가

--- api/src/main/java/com/commerce/saleday/api/infra/database/repository/outbox/OutboxJpaRepository.java
+++ api/src/main/java/com/commerce/saleday/api/infra/database/repository/outbox/OutboxJpaRepository.java
@@ -1,8 +1,12 @@
 package com.commerce.saleday.api.infra.database.repository.outbox;
 
 import com.commerce.saleday.domain.outbox.model.OutboxMessage;
+import com.commerce.saleday.domain.outbox.model.OutboxStatus;
+import java.time.LocalDateTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OutboxJpaRepository extends JpaRepository<OutboxMessage, Long> {
 
+  //spring data jpa 장점 사용
+  void deleteByStatusAndCreatedAtBefore(OutboxStatus outboxStatus, LocalDateTime oneMonthAgo);
 }
