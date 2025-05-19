1bf5986e2e4f5e510fb5dbf8869862b0ba668634
chyn00 <ybj0749@gmail.com>
2025-05-19 21:31:43 +0900

    add. kakaopay client 개발

--- pay/src/main/java/com/commerce/saleday/pay/infra/external/KakaoPay.java
+++ pay/src/main/java/com/commerce/saleday/pay/infra/external/KakaoPay.java
@@ -1,5 +1,13 @@
 package com.commerce.saleday.pay.infra.external;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@RequiredArgsConstructor
 public class KakaoPay {
+
+  private final RestClient restClient;
 
 }
