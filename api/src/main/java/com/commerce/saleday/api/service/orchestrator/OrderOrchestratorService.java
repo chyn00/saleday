c23cb339754e40da68dfe26e6accc77ddec7ce32
chyn00 <ybj0749@gmail.com>
2025-07-19 20:59:56 +0900

    add. Old Exception 수정

--- api/src/main/java/com/commerce/saleday/api/service/orchestrator/OrderOrchestratorService.java
+++ api/src/main/java/com/commerce/saleday/api/service/orchestrator/OrderOrchestratorService.java
@@ -1,6 +1,8 @@
 package com.commerce.saleday.api.service.orchestrator;
 
 import com.commerce.saleday.api.controller.order.model.OrderRequestDto;
+import com.commerce.saleday.common.exception.ExceptionCode;
+import com.commerce.saleday.common.exception.SaleDayException;
 import com.commerce.saleday.order.domain.stock.port.ItemStockPublisherKafkaPort;
 import com.commerce.saleday.order.service.order.OrderService;
 import com.commerce.saleday.order.service.stock.ItemStockService;
@@ -30,7 +32,7 @@ public class OrderOrchestratorService {
 
     if (remaining < 0) {
       itemStockService.incrementAndCountItemStock(orderRequestDto.getItemCode());
-      throw new Exception("재고가 부족합니다");// todo : exception 추후 일괄 수정
+      throw new SaleDayException(ExceptionCode.OUT_OF_STOCK);
     }
 
     try {
